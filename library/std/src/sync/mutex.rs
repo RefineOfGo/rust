c438214db8f01f45293b6d12dc8e852681c0d8ec
chenzhuoyu <chenzhuoyu1992@gmail.com>
2024-10-22 17:27:25 +0800

    chore: add missing lock guards

--- library/std/src/sync/mutex.rs
+++ library/std/src/sync/mutex.rs
@@ -215,6 +215,10 @@ pub struct MutexGuard<'a, T: ?Sized + 'a> {
     poison: poison::Guard,
 }
 
+#[cfg(not(bootstrap))]
+#[stable(feature = "rog", since = "1.0.0")]
+impl<T: ?Sized + Managed> Managed for MutexGuard<'_, T> {}
+
 #[stable(feature = "rust1", since = "1.0.0")]
 impl<T: ?Sized> !Send for MutexGuard<'_, T> {}
 #[stable(feature = "mutexguard", since = "1.19.0")]
@@ -255,6 +259,10 @@ pub struct MappedMutexGuard<'a, T: ?Sized + 'a> {
     poison: poison::Guard,
     _variance: PhantomData<&'a mut T>,
 }
+
+#[cfg(not(bootstrap))]
+#[stable(feature = "rog", since = "1.0.0")]
+impl<T: ?Sized + Managed> Managed for MappedMutexGuard<'_, T> {}
 
 #[unstable(feature = "mapped_lock_guards", issue = "117108")]
 impl<T: ?Sized> !Send for MappedMutexGuard<'_, T> {}
